ff70a5c0a274b7b270f19b25f3535c1707bf35ec
htrbao <trbaohoo@gmail.com>
2024-02-06 00:25:04 +0700

    [baoht] caption style transfer

---
+++ caption_style_transfer/run.sh
@@ -0,0 +1,24 @@
+export LOG_DIR=./logs
+export DATA_DIR=./datasets
+export TASK_NAME=ai_city_challenge
+export MODEL=caption_style_transfer
+export TEXT_COLUMN=sentence
+export SUMMARY_COLUMN=gt
+export DATA_RATIO=1
+export DATASET_NAME=ai_city_challenge
+export NLPEXP=./output
+mkdir -p $DATA_DIR/$TASK_NAME/$DATA_RATIO/$MODEL
+mkdir -p $LOG_DIR/$TASK_NAME/$DATA_RATIO/
+touch $LOG_DIR/$TASK_NAME/$DATA_RATIO/$MODEL
+python run.py \
+    --model_name_or_path facebook/bart-base \
+    --do_train \
+    --do_eval \
+    --train_file $DATA_DIR/BART_denoising_data/train.csv \
+    --validation_file $DATA_DIR/BART_denoising_data/val.csv \
+    --source_prefix "paraphase: " \
+    --output_dir $DATA_DIR/$TASK_NAME/$DATA_RATIO/$MODEL \
+    --overwrite_output_dir \
+    --per_device_train_batch_size=4 \
+    --per_device_eval_batch_size=4 \
+    --predict_with_generate
---
+++ pattern_extractor/main.py
@@ -0,0 +1,8 @@
+import json
+import os
+
+def patternize(file_path: str, type: str = "<pos>"):
+    """
+    
+    """
+    for x in os.walk(file_path):
