20f9abbc9b8a1237c77f58466322eeb78128bf12
ToTuanAn <totuanan06@gmail.com>
2024-03-10 01:12:50 +0700

    debug

--- internvid/modules/until_config.py
+++ internvid/modules/until_config.py
@@ -39,6 +39,7 @@ class PretrainedConfig(object):
 
     @classmethod
     def get_config(cls, pretrained_model_name, cache_dir, type_vocab_size, state_dict, task_config=None):
+        print("haha")
         archive_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), pretrained_model_name)
         if os.path.exists(archive_file) is False:
             if pretrained_model_name in cls.pretrained_model_archive_map:
