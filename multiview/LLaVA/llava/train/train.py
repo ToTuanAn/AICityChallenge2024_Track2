2c9dd0bda6dec9470cdb6b727b861f29ad8bdf91
Nguyễn Quang Tân <81596255+nguyenquangtan@users.noreply.github.com>
2024-04-28 05:21:43 +0700

    fix training

--- multiview/LLaVA/llava/train/train.py
+++ multiview/LLaVA/llava/train/train.py
@@ -932,7 +932,7 @@ def train(attn_implementation=None):
                 cache_dir=training_args.cache_dir,
                 attn_implementation=attn_implementation,
                 # torch_dtype=(torch.bfloat16 if training_args.bf16 else None),
-                torch_dtype=(torch.float16 if training_args.fp16 else (torch.bfloat16 if training_args.bf16 else None)) # avoid torch.float32
+                torch_dtype=(torch.float16 if training_args.fp16 else (torch.bfloat16 if training_args.bf16 else None)), # avoid torch.float32
                 **bnb_model_from_pretrained_args
             )
     else:
